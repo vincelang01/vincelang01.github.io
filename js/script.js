3799a918fcfa8b49b0e9ca44e7c572943ff6ca24
vincelang01 <lang.vince07@gmail.com>
2023-06-29 11:49:11 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,41 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var slides = document.querySelectorAll(".slide");
+    var currentSlide = 0;
+    var prev = document.querySelector(".prev");
+    var next = document.querySelector(".next");
+    var interval;
+  
+    function showSlide(n) {
+      clearInterval(interval);
+      slides[currentSlide].classList.remove("active");
+      currentSlide = (n + slides.length) % slides.length;
+      slides[currentSlide].classList.add("active");
+      startSlideshow();
+    }
+  
+    function startSlideshow() {
+      interval = setInterval(function() {
+        // Hozzáadunk egy tranzíciót a kép váltáshoz
+        slides[currentSlide].classList.add("transition");
+        setTimeout(function() {
+          slides[currentSlide].classList.remove("transition");
+        }, 1000); // Az animáció ideje
+  
+        showSlide(currentSlide + 1);
+      }, 7000);
+    }
+  
+    prev.addEventListener("click", function(e) {
+      e.preventDefault();
+      showSlide(currentSlide - 1);
+    });
+  
+    next.addEventListener("click", function(e) {
+      e.preventDefault();
+      showSlide(currentSlide + 1);
+    });
+  
+    slides[currentSlide].classList.add("active"); // Az első kép aktiválása
+    startSlideshow();
+  });
+  
